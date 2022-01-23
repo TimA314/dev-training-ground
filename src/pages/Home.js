f2fbee382b5ff401cb0977ce6b6f07720a3a51f9
TimA314 <ci0d7j9m@mailer.me>
2022-01-23 08:37:51 -0800

    add simple layout to Home component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,24 @@
 import React from "react";
-
+import cloneRepoImage from "./../assets/images/cloneRepoImage.jpg";
+import routesImage from "./../assets/images/routesImage.jpg";
 export default function Home() {
-  return <h1>Dev Training Grounds</h1>;
+  return (
+    <>
+      <h1>Dev Training Grounds</h1>
+      <p>
+        Put your skills to the test and help build a project with the community.
+      </p>
+      <div>
+        <h3>Getting Started:</h3>
+        <ul>
+          <li>clone this repo</li>
+          <img src={cloneRepoImage} alt="screenshot" width={300} />
+          <li>Create a file under projects</li>
+          <li>Make sure to add a route in the app.js</li>
+          <img src={routesImage} alt="screenshot" width={300} />
+          <li>Build whatever you like</li>
+        </ul>
+      </div>
+    </>
+  );
 }
