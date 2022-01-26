1e52e44bd6b33bf142ebe70616a96c3bb5252653
TimA314 <ci0d7j9m@mailer.me>
2022-01-25 19:29:20 -0800

    rearranged files

--- src/components/TutorialCard.js
+++ src/components/TutorialCard.js
@@ -1,16 +1,29 @@
 import React from "react";
-import "../css/TutorialCard.css";
+import "./TutorialCard.css";
 
 const TutorialCard = () => {
-    return (
-        <>
-            <div className="card">
-                <h4 className="card-title">Introduction to OOPS in C++</h4>
-                <p className="mt-3">Know the basics of C++ , now looking forward to learn object oriented programming, to ace your interviews and college tests ? Then, this is the perfect track for you as we cover here OOPS from the basics to advanced. We have mcqs, interview questions and coding problems to complement your journey. </p>
-                <a target="_blank" class="btn btn-primary mt-auto" href="https://www.codingninjas.com/codestudio/guided-paths/basics-of-c/content/118817/offering/1381799" role="button">Learn More</a>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="card">
+        <h4 className="card-title">Introduction to OOPS in C++</h4>
+        <p className="mt-3">
+          Know the basics of C++ , now looking forward to learn object oriented
+          programming, to ace your interviews and college tests ? Then, this is
+          the perfect track for you as we cover here OOPS from the basics to
+          advanced. We have mcqs, interview questions and coding problems to
+          complement your journey.{" "}
+        </p>
+        <a
+          rel="_blank"
+          class="btn btn-primary mt-auto"
+          href="https://www.codingninjas.com/codestudio/guided-paths/basics-of-c/content/118817/offering/1381799"
+          role="button"
+        >
+          Learn More
+        </a>
+      </div>
+    </>
+  );
+};
 
 export default TutorialCard;
