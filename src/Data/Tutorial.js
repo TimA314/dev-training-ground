4a687a489dadbb17c236bc547cdbba4494a1b342
Ritik Saxena <ritiksaxena124@gmail.com>
2022-01-27 15:43:29 +0530

    Links Added

--- src/Data/Tutorial.js
+++ src/Data/Tutorial.js
@@ -1,7 +1,47 @@
 export const Tutorial = [
     {
-        heading: "Introduction to OOPS in C++",
+        heading: "Introduction to C++",
+        desc: "If you are wondering how to start programming in the C++ language, here is your ultimate guide for practising and testing your problem-solving skills. Enrol in this C++ guided path to learn the basics of programming like functions, loops, arrays, strings etc.",
+        link:"https://www.codingninjas.com/codestudio/guided-paths/basics-of-c/content/118623/offering/1377972"
+    },
+    {
+        heading: "Introduction to OOPs in C++",
         desc: "Know the basics of C++ , now looking forward to learn object oriented programming, to ace your interviews and college tests ? Then, this is the perfect track for you as we cover here OOPS from the basics to advanced. We have mcqs, interview questions and coding problems to complement your journey.",
         link:"https://www.codingninjas.com/codestudio/guided-paths/oops-in-c/content/238489/offering/3210642"
+    },
+    {
+        heading: "Introduction to Data Structures & Algorithms",
+        desc: "If you are wondering how to prepare data structures and algorithms to do well in your programming interviews, here is your ultimate guide for practising and testing your problem-solving skills. Enroll in this data structure and algorithms path to learn data structures like Hash Tables, Trees, Tries, Graphs, and various algorithms.",
+        link:"https://www.codingninjas.com/codestudio/guided-paths/data-structures-algorithms/content/118820/offering/1381860"
+    },
+    {
+        heading: "Introduction to C++",
+        desc: "If you are wondering how to start programming in the C++ language, here is your ultimate guide for practising and testing your problem-solving skills. Enrol in this C++ guided path to learn the basics of programming like functions, loops, arrays, strings etc.",
+        link:"https://www.codingninjas.com/codestudio/guided-paths/basics-of-c/content/118623/offering/1377972"
+    },
+    {
+        heading: "Introduction to OOPs in C++",
+        desc: "Know the basics of C++ , now looking forward to learn object oriented programming, to ace your interviews and college tests ? Then, this is the perfect track for you as we cover here OOPS from the basics to advanced. We have mcqs, interview questions and coding problems to complement your journey.",
+        link:"https://www.codingninjas.com/codestudio/guided-paths/oops-in-c/content/238489/offering/3210642"
+    },
+    {
+        heading: "Introduction to Data Structures & Algorithms",
+        desc: "If you are wondering how to prepare data structures and algorithms to do well in your programming interviews, here is your ultimate guide for practising and testing your problem-solving skills. Enroll in this data structure and algorithms path to learn data structures like Hash Tables, Trees, Tries, Graphs, and various algorithms.",
+        link:"https://www.codingninjas.com/codestudio/guided-paths/data-structures-algorithms/content/118820/offering/1381860"
+    },
+    {
+        heading: "Introduction to C++",
+        desc: "If you are wondering how to start programming in the C++ language, here is your ultimate guide for practising and testing your problem-solving skills. Enrol in this C++ guided path to learn the basics of programming like functions, loops, arrays, strings etc.",
+        link:"https://www.codingninjas.com/codestudio/guided-paths/basics-of-c/content/118623/offering/1377972"
+    },
+    {
+        heading: "Introduction to OOPs in C++",
+        desc: "Know the basics of C++ , now looking forward to learn object oriented programming, to ace your interviews and college tests ? Then, this is the perfect track for you as we cover here OOPS from the basics to advanced. We have mcqs, interview questions and coding problems to complement your journey.",
+        link:"https://www.codingninjas.com/codestudio/guided-paths/oops-in-c/content/238489/offering/3210642"
+    },
+    {
+        heading: "Introduction to Data Structures & Algorithms",
+        desc: "If you are wondering how to prepare data structures and algorithms to do well in your programming interviews, here is your ultimate guide for practising and testing your problem-solving skills. Enroll in this data structure and algorithms path to learn data structures like Hash Tables, Trees, Tries, Graphs, and various algorithms.",
+        link:"https://www.codingninjas.com/codestudio/guided-paths/data-structures-algorithms/content/118820/offering/1381860"
     },
 ];
