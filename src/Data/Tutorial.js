4d44680a60a5c37cc4a85f0ef39cb04bb1a203b3
Ritik Saxena <ritiksaxena124@gmail.com>
2022-01-27 15:32:39 +0530

    ADD: Page load transition and Data file

---
+++ src/Data/Tutorial.js
@@ -0,0 +1,7 @@
+export const Tutorial = [
+    {
+        heading: "Introduction to OOPS in C++",
+        desc: "Know the basics of C++ , now looking forward to learn object oriented programming, to ace your interviews and college tests ? Then, this is the perfect track for you as we cover here OOPS from the basics to advanced. We have mcqs, interview questions and coding problems to complement your journey.",
+        link:"https://www.codingninjas.com/codestudio/guided-paths/oops-in-c/content/238489/offering/3210642"
+    },
+];
--- src/components/TutorialCard.js
+++ src/components/TutorialCard.js
@@ -1,27 +1,26 @@
 import React from "react";
 import "./TutorialCard.css";
+import { Bounce } from "react-awesome-reveal";
 
-const TutorialCard = () => {
+const TutorialCard = (props) => {
   return (
     <>
-      <div className="card">
-        <h4 className="card-title">Introduction to OOPS in C++</h4>
-        <p >
-          Know the basics of C++ , now looking forward to learn object oriented
-          programming, to ace your interviews and college tests ? Then, this is
-          the perfect track for you as we cover here OOPS from the basics to
-          advanced. We have mcqs, interview questions and coding problems to
-          complement your journey.
-        </p>
-        <a
-          rel="_blank"
-          class="btn"
-          href="https://www.codingninjas.com/codestudio/guided-paths/basics-of-c/content/118817/offering/1381799"
-          role="button"
-        >
-          Learn More
-        </a>
-      </div>
+      <Bounce triggerOnce="true">
+        <div className="card">
+          <h4 className="card-title">{props.heading}</h4>
+          <p >
+            {props.desc}
+          </p>
+          <a
+            rel="_blank"
+            className="btn"
+            href={props.link}
+            role="button"
+          >
+            Learn More
+          </a>
+        </div>
+      </Bounce>
     </>
   );
 };
